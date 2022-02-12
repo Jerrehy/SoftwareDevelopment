d55026e38c020e5563ec77217a135089eb489b3c
Jerrehy <sacha15012000@gmail.com>
2022-02-12 16:13:28 +0300

    Minor update 6

--- software_app/config.py
+++ software_app/config.py
@@ -1,10 +1,10 @@
 class Config:
     # Данные конфигурации подключения к БД
     dialect = 'postgresql'
-    username = 'postgres'
-    password = 'Qwerty7'
+    username = 'pdvlnepwqpjzkz'
+    password = '02a1ddc0bcf4e929d9171c6e4f253a50d331272bbf9d86c8e30ac6ab1862bdf8'
     host = 'ec2-63-32-30-191.eu-west-1.compute.amazonaws.com'
-    db_name = 'software_development'
+    db_name = 'degbd0d006h529'
 
     # Настройки для экземляра: секретный ключ запуска и путь к БД
     SQLALCHEMY_DATABASE_URI = f'{dialect}://{username}:{password}@{host}/{db_name}'
